6af1b205c980bd7a69b653c04c941c6f278e8fb5
Yeshwanth.J <yeshwanth.yeshu07@gmail.com>
2021-01-21 20:19:49 +0530

    Add files via upload

---
+++ Q3.py
@@ -0,0 +1,12 @@
+test_str = "ChennaiSuperKings"
+
+
+count = 0
+
+for i in test_str:
+    if i == 'e':
+        count = count + 1
+
+# printing result
+print("Count of e in ChennaiSuperKings is : "
+      + str(count))
