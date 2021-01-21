457c22ccb3bd83f1a1d58f86c0f4a274295e5fc3
Yeshwanth.J <yeshwanth.yeshu07@gmail.com>
2021-01-21 20:20:46 +0530

    Delete Q1.py

--- Q1.py
+++
@@ -1,4 +0,0 @@
-test_list = [5, 6, [], 3, [], [], 9]
-print("The original list is : " + str(test_list))
-res = [ele for ele in test_list if ele != []]
-print("List after empty list removal : " + str(res))
